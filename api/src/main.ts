fa053698c4740187cff7d396e041f346febc659e
JuanLima10 <juanaraujo051@gmail.com>
2026-04-05 16:31:33 -0300

    Refactor: New database architecture

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,29 @@
+import "dotenv/config";
+
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  )
+
+  const config = new DocumentBuilder()
+    .setTitle('IPlayed API')
+    .setDescription('API for IPlayed')
+    .setVersion('0.0.2')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api-docs', app, document)
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
