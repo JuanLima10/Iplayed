81c6f7aac2cc43b95881512070bae9beb793b94a
JuanLima10 <juanaraujo051@gmail.com>
2026-04-24 15:03:33 -0300

    v1.2.9

--- api/src/main.ts
+++ api/src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('IPlayed API')
     .setDescription('API for IPlayed')
-    .setVersion('v1.2.8')
+    .setVersion('v1.2.9')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
